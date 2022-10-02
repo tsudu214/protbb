abeedd4b486dc524bb2d0556621df5d440bcf5d9
tsudu214 <tsudu214@gmail.com>
2022-10-03 04:22:59 +0900

    chap02(~fig2_8)

--- ch02/fig_2_06.cpp
+++ ch02/fig_2_06.cpp
@@ -54,7 +54,7 @@ void f(int v) {
 }
 
 static void warmupTBB() {
-  tbb::parallel_for(0, tbb::task_scheduler_init::default_num_threads(), [](int) {
+  tbb::parallel_for(0, tbb::info::default_concurrency(), [](int) {
     tbb::tick_count t0 = tbb::tick_count::now();
     while ((tbb::tick_count::now() - t0).seconds() < 0.01);
   });
